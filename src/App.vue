d53dc7f6313fe69204fb3684eafb8314eabd5d11
cathero08 <ftootooo@gmail.com>
2022-10-24 01:28:46 +0800

    jkl

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import iii from '@/views/iii.vue'
 </script>
 
 <template>
   <div>890</div>
-  <RouterView />
+  <iii></iii>
+  <router-view />
 </template>
 
 <style scoped>
---
+++ src/views/iii.vue
@@ -0,0 +1,11 @@
+<template>
+
+  <div>看得到嗎</div>
+
+</template>
+
+<script  lang="ts" setup>
+
+</script>
+<style lang='scss' scope>
+</style>
